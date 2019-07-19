335982ef80c6dcab2f251c0ba9d99717c41d1bc2
Arvind Gupta <arbindbond222>
2019-07-19 09:41:09 +0530

    I am packaging .java file to send it to github

---
+++ BirthdayCakesCandle.java
@@ -0,0 +1,32 @@
+package Practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BirthdayCakesCandle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int no_of_candles=sc.nextInt();
+		int candles_height[]=new int[no_of_candles];
+		for(int i=0;i<candles_height.length;i++){
+			candles_height[i]=sc.nextInt();
+			
+		}
+		
+		
+		Arrays.sort(candles_height);
+		int blow=candles_height[candles_height.length-1];
+		int count=1;
+		for(int i=candles_height.length-2;i>=0;i--){
+			if(candles_height[i]==blow){
+				count++;
+			}
+			
+		}
+		System.out.println(count);
+
+	}
+
+}
